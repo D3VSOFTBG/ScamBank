f5402718d0da65026ac897bc919fc2df935c1195
D3VSOFTBG <59423787+D3VSOFTBG@users.noreply.github.com>
2024-04-27 21:02:40 +0300

    first commit

---
+++ src/main/java/com/d3vsoft/scambank/ScambankApplication.java
@@ -0,0 +1,13 @@
+package com.d3vsoft.scambank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScambankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScambankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/d3vsoft/scambank/ScambankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d3vsoft.scambank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScambankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
